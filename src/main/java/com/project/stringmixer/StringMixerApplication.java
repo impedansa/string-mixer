1b6c040cd22012bd21bd3d6953462a2819930b21
Natasha Milanov <>
2024-01-21 16:45:10 +0100

    Initial project setup.

---
+++ src/main/java/com/project/stringmixer/StringMixerApplication.java
@@ -0,0 +1,13 @@
+package com.project.stringmixer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StringMixerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StringMixerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/stringmixer/StringMixerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.stringmixer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StringMixerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
